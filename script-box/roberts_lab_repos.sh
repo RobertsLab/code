7b80e34d392ddabf5dc05322e175d6a78a131e04
sam <samwhite@uw.edu>
2017-08-15 21:06:03 -0700

    create directories

--- script-box/roberts_lab_repos.sh
+++ script-box/roberts_lab_repos.sh
@@ -2,3 +2,11 @@
 
 # Bash script for automatically retrieving all the Roberts Lab repos
 # and their corresponding wikis.
+
+mkdir ~/gitrepos
+
+mkdir ~/gitrepos/RobertsLab
+
+cd ~/gitrepos/RobertsLab
+
+

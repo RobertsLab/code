1b5ac0ffcaedbebe98342834bc90fd48501920e6
Sam White <samuel.j.white@gmail.com>
2019-08-05 10:49:57 -0700

    finish script

--- script-box/notebook_backups_github_gannet.sh
+++ script-box/notebook_backups_github_gannet.sh
@@ -1,6 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
 # Shell script to backup lab notebooks hosted on GitHub.
 
 # Notebook path
 notebook_dir="/volume2/web/github_backups/notebooks"
+
+# Array of notebooks
+notebooks_array=("${notebook_dir}/grace-ac.github.io" "${notebook_dir}/LabNotebook" "${notebook_dir}/sams-notebook" "${notebook_dir}/shellytrigg.github.io" "${notebook_dir}/sr320.github.io" "${notebook_dir}/yaaminiv.github.io")
+
+# Loop through notebook dirs and pull any repo updates
+for notebook in "${notebooks_array[@]}"
+do
+	cd "${notebook}" || exit
+	git pull
+	cd "${notebook_dir}" || exit
+done

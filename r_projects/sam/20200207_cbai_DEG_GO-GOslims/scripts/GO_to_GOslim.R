dba4098069a0cd2b49781ecc51ec5cd9863414fe
Sam White <samuel.j.white@gmail.com>
2020-02-11 15:33:03 -0800

    improve commenting

--- r_projects/sam/20200207_cbai_DEG_GO-GOslims/scripts/GO_to_GOslim.R
+++ r_projects/sam/20200207_cbai_DEG_GO-GOslims/scripts/GO_to_GOslim.R
@@ -1,6 +1,7 @@
 library(GSEABase)
 library(tidyverse)
 
+### Download files
 download.file(url = "https://gannet.fish.washington.edu/Atumefaciens/20200207_cbai_DEG/infected-vs-uninfected/edgeR.2317.dir/salmon.gene.counts.matrix.infected_vs_uninfected.edgeR.DE_results.P0.05_C1.infected-UP.subset.GOseq.enriched",
               destfile = "./data/salmon.gene.counts.matrix.infected_vs_uninfected.edgeR.DE_results.P0.05_C1.infected-UP.subset.GOseq.enriched")
 download.file(url = "https://gannet.fish.washington.edu/Atumefaciens/20200207_cbai_DEG/infected-vs-uninfected/edgeR.2317.dir/salmon.gene.counts.matrix.infected_vs_uninfected.edgeR.DE_results.P0.05_C1.infected-UP.subset.GOseq.depleted",
@@ -10,39 +11,47 @@ download.file(url = "https://gannet.fish.washington.edu/Atumefaciens/20200207_cb
 download.file(url = "https://gannet.fish.washington.edu/Atumefaciens/20200207_cbai_DEG/infected-vs-uninfected/edgeR.2317.dir/salmon.gene.counts.matrix.infected_vs_uninfected.edgeR.DE_results.P0.05_C1.uninfected-UP.subset.GOseq.depleted",
               destfile = "./data/salmon.gene.counts.matrix.infected_vs_uninfected.edgeR.DE_results.P0.05_C1.uninfected-UP.subset.GOseq.depleted")
 
+### Create list of files
 goseq_files <- list.files(path = "./data",
                          pattern = "\\.GOseq.[de]",
                          full.names = TRUE)
-  
+
+### Set output filename suffix
 output_suffix=("GOslims.csv")
 
-
-
-# Set full paths for goseq files
+### Strip path from goseq files
 goseq_filename <- basename(goseq_files)
-  
+
+
+
 for (item in goseq_files) {
   
-  print(item)
-  
+  ## Get max number of fields
+  # Needed to handle reading in file with different number of columns in each row
   max_fields <- max(count.fields(item, sep = "\t"))
   
-  # Read in tab-delimited GOseq file
+  ## Read in tab-delimited GOseq file
+  # Use "max_fields" to populate all columns with a sequentially numbered header
   go_seqs <- read.table(item,
                         sep = "\t",
                         header = TRUE,
                         col.names = paste0("V",seq_len(max_fields)),
                         fill = TRUE)
   
-  # Grab just the individual GO terms (i.e. "category" column)
+  ## Grab just the individual GO terms from the "category" column)
   goterms <- as.character(go_seqs$category)
   
-  #goslims with GSEA
+  ### Use GSEA to map GO terms to GOslims
+  
+  ## Store goterms as GSEA object
   myCollection <- GOCollection(goterms)
   
-  #I downloaded goslim_generic.obo from http://geneontology.org/docs/go-subset-guide/
-  #then i moved it to the R library for GSEABase in the extdata folder
-  slim <- getOBOCollection("~/Downloads/goslim_generic.obo")
+  ## Use generic GOslim file to create a GOslim collection
+  
+  # I downloaded goslim_generic.obo from http://geneontology.org/docs/go-subset-guide/
+  # then i moved it to the R library for GSEABase in the extdata folder
+  # in addition to using the command here - I think they're both required.
+  slim <- getOBOCollection("./data/goslim_generic.obo")
   
   # Select Biological Processes groups
   slims <- goSlim(myCollection, slim, "BP")

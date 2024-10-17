40fa66266dcc9dea082b050f1f4cd8a1e1554a75
kubu4 <samuel.j.white@gmail.com>
2024-10-17 13:58:31 -0700

    initial commit r_packages_installs.R

---
+++ apptainer_definition_files/r_packages_installs.R
@@ -0,0 +1,31 @@
+# Update base packages
+update.packages(ask = FALSE)
+
+# Install BioConductor package manager
+if (!requireNamespace("BiocManager", quietly = TRUE))
+install.packages("BiocManager")
+BiocManager::install(version = "3.19")
+
+# Install tidyverse
+install.packages("tidyverse")
+
+# Install matrixStats 0.61.0 (needed for DESeq2)
+install.packages("https://cran.rstudio.com/src/contrib/matrixStats_0.61.0.tar.gz", repos=NULL, type="source")
+
+# Install remotes package (allows for package installs from GitHub)
+BiocManager::install("remotes")
+
+# Install GSEABase (a dependency for numerous gene ontology/enrichment analysis)
+BiocManager::install("Bioconductor/GSEABase")
+
+# Install qvalue package
+BiocManager::install("qvalue")
+
+# Install GO.db (annotation maps for Gene Ontology data)
+BiocManager::install("GO.db")
+
+# Install MatrixGenerics (needed for DESeq2)
+BiocManager::install("MatrixGenerics")
+
+# Install DESeq2
+BiocManager::install("DESeq2")

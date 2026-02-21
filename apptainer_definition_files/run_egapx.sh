db7deff9e82afc624251c68a29096647d5683aa1
kubu4 <samuel.j.white@gmail.com>
2026-02-21 13:01:43 -0800

    initial commit run_egapx.sh

---
+++ apptainer_definition_files/run_egapx.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Simplified EGAPx runner - container has singularity installed internally
+
+# Make cache directory if it doesn't exist
+mkdir --parents /tmp/singularity_cache
+
+# Set NXF_SINGULARITY_CACHEDIR to the cache directory
+export NXF_SINGULARITY_CACHEDIR=/tmp/${USER}/singularity_cache
+
+# Run the EGAPx container with the provided arguments
+singularity run --cleanenv srlab-NCBI-EGAPx.sif "$@"

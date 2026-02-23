4e0963a6724770a9d54c197734022ef0ab5e5de9
kubu4 <samuel.j.white@gmail.com>
2026-02-23 12:20:48 -0800

    fix tmp singularity path

--- apptainer_definition_files/run_egapx.sh
+++ apptainer_definition_files/run_egapx.sh
@@ -2,7 +2,7 @@
 # Simplified EGAPx runner - container has singularity installed internally
 
 # Make cache directory if it doesn't exist
-mkdir --parents /tmp/singularity_cache
+mkdir --parents /tmp/${USER}/singularity_cache
 
 # Set NXF_SINGULARITY_CACHEDIR to the cache directory
 export NXF_SINGULARITY_CACHEDIR=/tmp/${USER}/singularity_cache
